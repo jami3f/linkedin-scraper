dff899e07d6d74a4788b2adb864873c037379bbd
jamie <jamie.fairhurst01@gmail.com>
2023-10-02 18:25:18 +0100

    Configured everything

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,69 @@
-import { Hono } from "hono";
-import { puppeteer } from "puppeteer";
+// import { Hono } from "hono";
+import puppeteer from "puppeteer";
+import { Browser } from "puppeteer";
 
-const browser = await puppeteer.launch();
+// const app = new Hono();
 
-const app = new Hono();
+// app.get("/", c => c.text("Hello Hono!"));
 
-app.get("/", c => c.text("Hello Hono!"));
+// app.post("/screenshot", async c => {
+// 	const browser = await puppeteer.launch({ headless: "new" });
+// 	const { url } = await c.req.json<{ url: string }>();
+// 	console.log(url);
+// 	console.log("screenshot");
+// 	const page = await browser.newPage();
+// 	await page.goto(url);
+// 	const buffer = await page.screenshot();
+// 	await page.close();
+// 	browser.close();
+// 	return c.stream(async s => {
+// 		await s.write(buffer);
+// 	});
+// });
+let browser: Browser;
+(async () => {
+	browser = await puppeteer.launch({ headless: false });
+	const page = (await browser.pages())[0];
+	await page.goto("https://www.linkedin.com/in/zahid-din-3787887/");
+	await page.waitForNavigation();
+	await page.click('button[data-tracking-control-name="auth_wall_desktop_profile-login-toggle"]');
+	await page.waitForSelector('button[data-id="sign-in-form__submit-btn"]');
+	const username = process.env.username;
+	const password = process.env.password;
+	if (username === undefined) throw new Error("Username not set");
+	if (password === undefined) throw new Error("Password not set");
+	await page.click('button[action-type="DENY"]');
+	await page.type("input[autocomplete='username']", username, { delay: 50 });
+	await page.type("input[autocomplete='current-password']", password, { delay: 50 });
+	await page.keyboard.press("Enter");
+	await page.waitForResponse("https://www.linkedin.com/feed/?trk=seo-authwall-base_sign-in-submit");
+	await page.goto("https://www.linkedin.com/in/zahid-din-3787887/");
+	const name = await page.$eval("h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words", el => el.textContent);
+	console.log("Name: " + name);
+	const bio = await page.$$eval("div[style='-webkit-line-clamp:4;'] > span", els =>
+		els.map(el => el.textContent).join("\n")
+	);
+	console.log("Bio:\n" + bio);
+	// console.log((await browser.pages()).map(p => p.url()));
+	// await page.click(".nsm7Bb-HzV7m-LgbsSe-BPrWId");
+	// const popupPromise = new Promise<Page>(x =>
+	// 	browser.once("targetcreated", async t => {
+	// 		const page = await t.page();
+	// 		if (page !== null) return x(page);
+	// 		throw new Error("Page is null");
+	// 	})
+	// );
+	// const loginPage = await popupPromise;
+	// await loginPage.waitForSelector("[type='email']");
+	// await loginPage.type("input[type='email']", "jamie.fairhurst01@gmail.com", { delay: 50 });
+	// await loginPage.keyboard.press("Enter");
+})();
 
-export default app;
+process.on("SIGINT", function () {
+	console.log("Caught interrupt signal");
+	browser.close();
+	process.kill(process.pid, "SIGINT");
+	process.exit();
+});
+
+// export default app;
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import "./App.css";
 
 function App() {
 	const [linkedInURL, setLinkedInURL] = useState<string>("");
+	const [person, setPerson] = useState<Person | undefined>({
+		name: "Zahid Din",
+		linkedInURL: "https://www.linkedin.com/in/zahid-din-3787887/",
+		bio: "Zahid is the technology evangelist at Computacenter and manager of the Solutions Center team. He is responsible for creating and demonstrating showcases along with the Solutions Center team. He is also responsible for the technical enablement of the sales team and customers. He has been working in the IT industry for over 20 years and has a wealth of experience in the design and implementation of IT solutions. He has worked in a variety of roles including pre-sales, consultancy, and support. He has a passion for technology and enjoys sharing his knowledge with others.",
+		email: "zahid.din@computacenter.com",
+		photoURL: "http://imageurl.com",
+	});
 
 	interface Person {
 		name: string;
@@ -12,22 +19,74 @@ function App() {
 		bio?: string;
 	}
 
-	function onSubmit() {}
+	async function onSubmit() {
+		const res = await fetch("/screenshot", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ url: linkedInURL }),
+		});
+		// const data = await res.arrayBuffer();
+		console.log(res.headers.get("Content-Type"));
+		const data = await res.blob();
+	}
 
 	return (
-		<div className="w-screen flex justify-center items-center gap-1">
-			<input
-				className="border-2 border-gray-300 bg-white h-10 w-96 px-2 rounded-lg text-sm focus:outline-none"
-				title="linkedInURL"
-				type="text"
-				onChange={e => setLinkedInURL(e.target.value)}
-			/>
-			<button
-				type="submit"
-				className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 h-8 rounded"
-			>
-				Submit
-			</button>
+		<div className="w-screen h-full flex items-center flex-col gap-6 py-2">
+			<div className="w-96 flex items-center gap-1">
+				<input
+					className="border-2 border-gray-300 bg-white h-10 w-96 px-2 rounded-lg text-sm focus:outline-none"
+					title="linkedInURL"
+					type="text"
+					onChange={e => setLinkedInURL(e.target.value)}
+				/>
+				<button
+					type="submit"
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 h-8 rounded"
+					onClick={onSubmit}
+				>
+					Submit
+				</button>
+			</div>
+			{person && (
+				<>
+					<div className="w-96 flex flex-col items-center">
+						<h1 className="font-bold">Name</h1>
+						<p>{person.name}</p>
+					</div>
+					<div className="w-96 flex flex-col items-center">
+						<h1 className="font-bold">Email</h1>
+						<p>{person.email}</p>
+					</div>
+					<div className="w-96 flex flex-col items-center">
+						<h1 className="font-bold">LinkedIn</h1>
+						<p>{person.linkedInURL}</p>
+					</div>
+					<div className="w-96 flex flex-col items-center">
+						<h1 className="font-bold">Photo</h1>
+						<img
+							alt="Profile pic"
+							src={"https://picsum.photos/300/300"}
+						/>
+					</div>
+					<div className="w-96 flex flex-col items-center">
+						<h1 className="font-bold">Bio</h1>
+						<textarea
+							title="bio"
+							className="border-2 border-gray-300 bg-white h-32 w-[50rem] px-2 rounded-lg text-sm focus:outline-none"
+							value={person.bio}
+						/>
+					</div>
+					<button
+						type="submit"
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 h-8 rounded"
+						// onClick={onSubmit}
+					>
+						Complete
+					</button>
+				</>
+			)}
 		</div>
 	);
 }
